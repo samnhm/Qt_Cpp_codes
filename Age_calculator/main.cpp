26d7be054e2a9a5a843119005df34bcd225d0bba
samnhm <147502842+samnhm@users.noreply.github.com>
2023-12-19 17:23:20 +0200

    Create main.cpp

---
+++ Age_calculator/main.cpp
@@ -0,0 +1,34 @@
+/*
+Age Calculater, using classes and encapsulation
+
+I put the class as 'QObject'
+*/
+
+#include <QCoreApplication>
+#include <QDebug>
+#include "agecalc.h"
+
+void print(AgeCalc &calc)
+{
+    qInfo() << calc.name() << "Human Years:" << calc.humanYears();
+    qInfo() << calc.name() << "Dog Years:" << calc.dogYears();
+    qInfo() << calc.name() << "Cat Years:" << calc.catYears();
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    AgeCalc saman;
+    AgeCalc limon;
+
+    saman.setName("Saman");
+    saman.setAge(25);
+    limon.setName("Limon");
+    limon.setAge(13);
+
+    print(saman);
+    print(limon);
+
+    return a.exec();
+}
